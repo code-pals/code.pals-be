c52ac6b43b3aa76334fd076c55ca84328064303a
AmitSheth9 <amit.sheth9@gmail.com>
2022-02-23 14:21:05 -0600

    recent changes to comments.test.js

---
+++ __tests__/comments.test.js
@@ -0,0 +1,37 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const Comment = require('../lib/models/Comment');
+
+describe('backend routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('should create a comment', async () => {
+    const res = await request(app)
+      .post('/api/v1/comments')
+      .send({
+        commenter: 1,
+        postId: 1,
+        comment: 'great question',
+        parent: null,
+        favorited: false,
+      });
+
+    expect(res.body).toEqual({
+      commentId: expect.any(String),   
+      commenter: 1,
+      postId: 1,
+      comment: 'great question',
+      parent: null,
+      favorited: false,
+      created: expect.any(String)
+    });
+  });
+});
