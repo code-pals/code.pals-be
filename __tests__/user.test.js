6a3be31d0c12bfbacdd40148c9767d02297698b4
Frank Hollywood <github@srvsync.com>
2022-03-07 13:00:05 -0800

    cleaned up code

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -27,14 +27,7 @@ describe('why-i-autha routes', () => {
       .agent(app)
       .get('/api/v1/users/login/callback?code=42')
       .redirects(1);
-    
+
     expect(res.status).toEqual(200);
-    // expect(req.body).toEqual({
-    //   user_id: expect.any(String),
-    //   github: 'ProsperieEli',
-    //   avatar: expect.any(String),
-    //   iat: expect.any(Number),
-    //   exp: expect.any(Number),
-    // });
   });
 });
